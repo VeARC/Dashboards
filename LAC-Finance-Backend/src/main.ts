8514a3ec1e315a6a8209075d0af7a87165bf5f99
krishna-vithal <skrishna@agileframeworks.com>
2023-02-23 17:56:12 +0530

    added web config file

--- LAC-Finance-Backend/src/main.ts
+++ LAC-Finance-Backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 5000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
